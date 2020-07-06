f3247b85a860d6aeede9ab9e9480b0a0517eac18
anjaligopi <anjaligopi23@gmail.com>
2020-07-05 23:57:11 -0700

    adapter str_lower, str_split

---
+++ adapter.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class String{
+  
+  public:
+    string s;
+
+  public:
+    
+    String(string inp){
+    s = inp;
+    }
+
+    String to_lower() const {
+      // constructor
+      //string dest(s);
+      string dest = s;
+      transform(s.begin(), s.end(), dest.begin(), ::tolower);
+      /*
+       * cout << "orig str:" << s << endl;
+      cout << dest << endl;
+      */
+      // constructor
+      // return dest also works, make it explicit/easier to understnd
+      // new String object is created - RAII - constructor called when u create an object and init the memory
+      // obj = String(dest) -> Python
+      String obj(dest);
+      return obj;
+    }
+
+    vector<string> split(char delim) const{
+      vector<string> dest;
+      stringstream ss(s);
+      string item;
+      vector<string> elems;
+      while (getline(ss, item, delim)) {
+         dest.push_back(item);
+         // elems.push_back(std::move(item)); // if C++11 (based on comment from @mchiasson)
+          }
+        return dest;
+    }
+};
+
+int main(){
+  string outp = {"Hello World"};
+  cout << outp << endl;
+  String obj(outp);
+  String _lower = obj.to_lower();
+  vector<string> _split = _lower.split(' ');
+  cout << "Lower case: " << _lower.s << endl;
+  //cout << "Split string: " << _split << endl;
+  for(auto &ch : _split){
+    cout << ch  << endl;
+  }
+}
